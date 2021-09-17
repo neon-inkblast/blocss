f6d41e30b7413397a0bfaccf614a71b2d23dfd57
Ben <ben.atkinson.official@gmail.com>
2021-09-17 19:10:43 +1000

    start timer for dropping

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,20 @@
 const dropBtn = document.getElementById("drop-btn");
 const dropper = document.querySelector(".bloc-dropper");
 let count = 1;
+let timerHandle = null; 
+let timerDelay = 500;
+
+
 function rndm(range) { 
     return Math.floor(Math.random() * range);
 }
 
-dropBtn.addEventListener("click", () => {
+function dropBox() {
     const width = dropper.offsetWidth;
     const colours = ["var(--c-primary)", "var(--c-secondary)", "var(--c-background)"];
     const colour = colours[rndm(3)];
     const x = rndm(width - 50);
-    const drift = 0//Math.round(rndm(width - x - 100) /2);
+    const drift = Math.round(rndm(width - x - 100) /2);
     
     const wrapper = document.createElement("div");
     wrapper.style.setProperty("--drift", drift + "px");
@@ -22,4 +26,17 @@ dropBtn.addEventListener("click", () => {
     newDiv.textContent = count++;
     dropper.append(wrapper);
     wrapper.append(newDiv);
-});
+}
+
+
+function startTimer() {
+    timerHandle = setInterval(() => {
+        dropBox();
+    }, timerDelay)
+}
+
+function stopTimer() {
+    clearInterval(timerHandle);
+}
+
+dropBtn.addEventListener("click", startTimer);
